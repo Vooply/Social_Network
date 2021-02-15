04041995e2ec9f0ad7c3693f688e6d9ac51ec712
Vooply <pv.yaschuk@gmail.com>
2021-02-15 17:34:06 +0200

    user app

--- src/app/users/urls.py
+++ src/app/users/urls.py
@@ -1,12 +1,11 @@
-from django.conf.urls import url
 from django.urls import path
 
-from app.users.views import CreateUserAPIView, AuthenticateUserAPIView, UserRetrieveUpdateAPIView
+from app.users.views import CreateUserAPIView, UserRetrieveUpdateAPIView, AuthenticateUserAPIView
 
 app_name = 'users'
 
 urlpatterns = [
     path('register/', CreateUserAPIView.as_view()),
     path('auth/', AuthenticateUserAPIView.as_view()),
-    path('update/', UserRetrieveUpdateAPIView.as_view())
+    path('update/', UserRetrieveUpdateAPIView.as_view()),
 ]
--- src/app/users/views.py
+++ src/app/users/views.py
@@ -1,10 +1,6 @@
-from urllib import request
-
 import jwt
 from django.contrib.auth import user_logged_in
-from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
@@ -19,39 +15,12 @@ from config import settings
 class CreateUserAPIView(APIView):
     permission_classes = (AllowAny,)
 
-    def post(self):
-        user = self.request.data
+    def post(self, request):
+        user = request.data
         serializer = UserSerializer(data=user)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-# @api_view(['POST'])
-# @permission_classes(["AllowAny", ])
-# def authenticate_user(request):
-#     try:
-#         email = request.data['email']
-#         password = request.data['password']
-#
-#         user = User.objects.get(email=email, password=password)
-#         if user:
-#             try:
-#                 payload = jwt_payload_handler(user)
-#                 token = jwt.encode(payload, settings.SECRET_KEY)
-#                 user_details = {'name': "%s" % user.username, 'token': token}
-#                 user_logged_in.send(sender=user.__class__, request=request, user=user)
-#                 return Response(user_details, status=status.HTTP_200_OK)
-#             except Exception as e:
-#                 raise e
-#         else:
-#             out = {
-#                 'error': 'can not authenticate with the given credentials or the account'
-#             }
-#             return Response(out, status=status.HTTP_403_FORBIDDEN)
-#     except KeyError:
-#         out = {'error': "Please provide an email and a password"}
-#         return Response(out)
 
 
 class AuthenticateUserAPIView(APIView):
@@ -59,24 +28,27 @@ class AuthenticateUserAPIView(APIView):
 
     def post(self, request):
         try:
-            email = self.request.data['email']
-            password = self.request.data['password']
+            email = request.data['email']
+            password = request.data['password']
 
             user = User.objects.get(email=email, password=password)
             if user:
+
                 try:
                     payload = jwt_payload_handler(user)
                     token = jwt.encode(payload, settings.SECRET_KEY)
-                    user_details = {'name': user.username, 'token': token}
+                    user_details = {'name': user.email, 'token': token}
                     user_logged_in.send(sender=user.__class__, request=request, user=user)
                     return Response(user_details, status=status.HTTP_200_OK)
                 except Exception as e:
                     raise e
+
             else:
                 out = {
                     'error': 'can not authenticate with the given credentials or the account'
                 }
                 return Response(out, status=status.HTTP_403_FORBIDDEN)
+
         except KeyError:
             out = {'error': "Please provide an email and a password"}
             return Response(out)

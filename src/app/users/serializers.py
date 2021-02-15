6fdb5c139fdab5a87b87c71f54d6680c72d783fb
Vooply <pv.yaschuk@gmail.com>
2021-02-15 17:45:42 +0200

    clean comment code

--- src/app/users/serializers.py
+++ src/app/users/serializers.py
@@ -10,24 +10,3 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'email', 'username', 'date_joined', 'password')
         extra_fields = {'password': {'write_only': True}}
-
-    # def validate_email(self, value):
-    #     user = self.context['request'].user
-    #     if User.objects.exclude(pk=user.pk).filter(email=value).exists():
-    #         raise serializers.ValidationError({"email": "This email is already in use."})
-    #     return value
-    #
-    # def validate_username(self, value):
-    #     user = self.context['request'].user
-    #     if User.objects.exclude(pk=user.pk).filter(username=value).exists():
-    #         raise serializers.ValidationError({"username": "This username is already in use."})
-    #     return value
-    #
-    # def update(self, instance, validated_data):
-    #     instance.email = validated_data['email']
-    #     instance.username = validated_data['username']
-    #     instance.password = validated_data['password']
-    #
-    #     instance.save()
-    #
-    #     return instance
